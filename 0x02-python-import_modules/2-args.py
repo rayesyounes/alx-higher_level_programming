01deec28cd9785e4e8f3bf66ec824cb3344de4e5
rayesyounes <rayesyounes88@gmail.com>
2023-06-08 06:19:26 -0700

    task-2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
-    import sys
-    args = sys.argv
-    num_args = len(args) - 1
-    if num_args == 0:
-        print("0 arguments.")
-    elif num_args == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(num_args))
-    for i in range(num_args):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+     import sys
+     args = sys.argv
+     num_args = len(args) - 1
+     if num_args == 0:
+         print("0 arguments.")
+     elif num_args == 1:
+         print("1 argument:")
+     else:
+         print("{} arguments:".format(num_args))
+     for i in range(num_args):
+         print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
