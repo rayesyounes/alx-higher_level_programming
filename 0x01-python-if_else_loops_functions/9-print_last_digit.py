b6da18d7dae9639c8a1fd76cddd613b4ff67a0e6
rayesyounes <rayesyounes88@gmail.com>
2023-08-08 03:44:33 -0700

    task-9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,5 +5,4 @@ def uppercase(str):
         if ord(ch) >= 97 and ord(ch) <= 122:
             ch = chr(ord(ch) - 32)
         print("{:s}".format(ch), end='')
-
-    print('\n', end="")
+print('\n', end="")
