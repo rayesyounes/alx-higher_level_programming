95254300a80a89222e002223e1ca462002d76d39
rayesyounes <rayesyounes88@gmail.com>
2023-11-18 19:21:24 +0100

    task-9 checker-4 fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
         pool_pre_ping=True,
     )
     with engine.connect() as connection:
-        query = select(State).order_by(State.id.asc()).where(State.name.like("%a%"))
+        query = select(State).order_by(State.id.asc()) \
+                .where(State.name.like("%a%"))
         states = connection.execute(query)
 
         for state in states:
