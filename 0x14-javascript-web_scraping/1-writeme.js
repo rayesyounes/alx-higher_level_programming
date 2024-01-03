838596160869f5a95430acc2a46af0dd2b44f95e
rayesyounes <rayesyounes88@gmail.com>
2024-01-03 22:59:01 +0100

    task-1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
+const fs = require("fs");
 const file = process.argv[2];
 const string = process.argv[3];
 try {
-  fs.writeFileSync(file, string, { encoding: 'utf8' });
+  fs.writeFileSync(file, string, { encoding:"utf8"});
 } catch (err) {
   console.error(err);
 }
